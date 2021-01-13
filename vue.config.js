dcbfbf545d758089c69f58f5a5094c29e4292ad5
Jettison200 <jettison200@gmail.com>
2021-01-12 21:04:35 -0500

    d

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ module.exports = {
     pwa: {
       workboxPluginMode: 'InjectManifest',
       workboxOptions: {
-        swSrc: './src/sw.js',
         swDest: 'service-worker.js',
       },
     },
